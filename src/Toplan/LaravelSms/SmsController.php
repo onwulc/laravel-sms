97de93110160fb1d647cf307548ed4bf1843a47d
lance.wu <lance.wu@flyertea.com>
2017-05-04 13:56:33 +0800

    update info html

--- src/Toplan/LaravelSms/SmsController.php
+++ src/Toplan/LaravelSms/SmsController.php
@@ -44,9 +44,6 @@ class SmsController extends Controller
     public function getInfo()
     {
         $html = '<meta charset="UTF-8"/><h2 align="center" style="margin-top: 30px;margin-bottom: 0;">Laravel Sms</h2>';
-        $html .= '<p style="margin-bottom: 30px;font-size: 13px;color: #888;" align="center">' . SmsManager::VERSION . '</p>';
-        $html .= '<p><a href="https://github.com/toplan/laravel-sms" target="_blank">laravel-sms源码</a>托管在GitHub，欢迎你的使用。如有问题和建议，欢迎提供issue。</p>';
-        $html .= '<hr>';
         $html .= '<p>你可以在调试模式(设置config/app.php中的debug为true)下查看到存储在存储器中的验证码短信/语音相关数据:</p>';
         echo $html;
         if (config('app.debug')) {
